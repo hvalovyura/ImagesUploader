3e0e41b625fd9f8796da1c1ba04784faac9cbbcd
Yura <hvalovyura@gmail.com>
2020-04-12 21:42:49 +0300

    Adding getters and setters into model

--- src/main/java/hvalov/domain/Photo.java
+++ src/main/java/hvalov/domain/Photo.java
@@ -17,4 +17,43 @@ public class Photo {
     private String imagePath;
     private List<String> comments;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public List<String> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<String> comments) {
+        this.comments = comments;
+    }
 }
