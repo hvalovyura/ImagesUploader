91ddeb2c79b3f113f32cb86769a162348f10ca1d
Yura <hvalovyura@gmail.com>
2020-04-12 22:34:57 +0300

    Adding main template atd correcting greeting controller (main method)

--- src/main/java/hvalov/GreetingController.java
+++ src/main/java/hvalov/GreetingController.java
@@ -1,5 +1,8 @@
 package hvalov;
 
+import hvalov.domain.Photo;
+import hvalov.repos.PhotoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,9 +11,20 @@ import java.util.Map;
 
 @Controller
 public class GreetingController {
-    @GetMapping
+
+    @Autowired
+    private PhotoRepo photoRepo;
+
+    @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
         model.put("name", name);
         return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model){
+        Iterable<Photo> photos = photoRepo.findAll();
+        model.put("photos", photos);
+        return "main";
     }
 }
