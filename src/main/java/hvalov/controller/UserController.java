a7cb5e0c54ed4bcc71acaf19705079fc0a11e54c
Yura <hvalovyura@gmail.com>
2020-04-25 15:12:31 +0300

    Refactoring from mustache to ftl

--- src/main/java/hvalov/controller/UserController.java
+++ src/main/java/hvalov/controller/UserController.java
@@ -1,10 +1,12 @@
 package hvalov.controller;
 
+import hvalov.domain.User;
 import hvalov.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +19,11 @@ public class UserController {
     public String userList(Model model) {
         model.addAttribute("users", userRepo.findAll());
         return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model) {
+        model.addAttribute("user", user);
+        return "userEdit";
     }
 }
