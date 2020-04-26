c2e1200ab61fee91cd89fa845a4201c8eeacf626
Yura <hvalovyura@gmail.com>
2020-04-26 17:09:56 +0300

    Adding error maping

--- src/main/java/hvalov/controller/MainController.java
+++ src/main/java/hvalov/controller/MainController.java
@@ -7,15 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
@@ -41,30 +47,38 @@ public class MainController {
     @PostMapping("/main")
     public String add(
             @AuthenticationPrincipal User user,
-            @RequestParam String name,
-            Map<String, Object> model,
+            @Valid Photo photo,
+            BindingResult bindingResult,
+            Model model,
             @RequestParam("file") MultipartFile file
             ) throws IOException {
-        Photo photo = new Photo(name, user);
+        photo.setAuthor(user);
 
-        if(file != null) {
-            File uploadDir = new File(uploadPath);
-            if(!uploadDir.exists()) {
-                uploadDir.mkdir();
-            }
-            String uuidFile = UUID.randomUUID().toString();
-            String resultFilename = uuidFile + "." + file.getOriginalFilename();
-
-            file.transferTo(new File(uploadPath + "/" + resultFilename));
-
-            photo.setFilename(resultFilename);
+        if(bindingResult.hasErrors()) {
+            Map<String, String> errorsMap = ControllerUtils.getErrors(bindingResult);
+            model.mergeAttributes(errorsMap);
         }
+        else {
+            if(file != null) {
+                File uploadDir = new File(uploadPath);
+                if(!uploadDir.exists()) {
+                    uploadDir.mkdir();
+                }
+                String uuidFile = UUID.randomUUID().toString();
+                String resultFilename = uuidFile + "." + file.getOriginalFilename();
 
+                file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+                photo.setFilename(resultFilename);
+            }
+        }
         photoRepo.save(photo);
 
         Iterable<Photo> photos = photoRepo.findAll();
-        model.put("photos", photos);
+        model.addAttribute("photos", photos);
 
         return "main";
     }
+
+
 }
